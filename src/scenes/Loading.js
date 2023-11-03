5d552488f60b609102cb317fa34fd259c98d3e39
An-geles <66815800+An-geles@users.noreply.github.com>
2023-11-03 10:38:35 -0700

    update: Loading.js now produces loading bar, added: prefabs for other sprites

---
+++ src/scenes/Loading.js
@@ -0,0 +1,27 @@
+class Loading extends Phaser.Scene {
+    constructor() {
+        super("loadingScene");
+    }
+
+    preload() {
+    //     I am using the implementation of the loading bar from Professors example in class,
+        //     I liked how the loading bar looked in the game
+        let loadingBar = this.add.graphics();
+
+        this.load.on('progress', (value) => {
+            loadingBar.clear();
+            loadingBar.fillStyle(0xFFFFF, 1);
+            loadingBar.fillRect(0, centerY, w * value, 5);
+        });
+
+        this.load.on('complete', () => {
+            loadingBar.destroy();
+        });
+
+        this.load.path = './assets/';
+    }
+
+    create() {
+        this.scene.start('menuScene');
+    }
+}
